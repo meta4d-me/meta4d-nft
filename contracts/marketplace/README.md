bcf135f9f9c5aa0cc9d3fb05a349bd28912c9ce3
etherCrossroads <45531468+etherCrossroads@users.noreply.github.com>
2022-02-28 11:14:19 +0800

    clear repo, add some example contract

--- contracts/marketplace/README.md
+++
@@ -1,5 +0,0 @@
-# Platwin NFT Marketplace
-
-Smart contracts only need to support Dutch auctions:
-
-- if min&max price is equal, the auction is one price;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,41 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
-const env = require('./.env.json');
 
-INFURO_API_KEY = env.INFURO;
-
-PRIV_1 = env.PRIVATE_KEY_1;
-PRIV_2 = env.PRIVATE_KEY_2;
-PRIV_3 = env.PRIVATE_KEY_3;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: {
-        version: '0.8.4',
+        version: '0.8.12',
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 999999
+                runs: 200
             }
         }
     },
     networks: {
-        ropsten: {
-            url: `https://ropsten.infura.io/v3/${INFURO_API_KEY}`,
-            accounts: [`0x${PRIV_1}`]
-        },
-        kovan: {
-            url: `https://kovan.infura.io/v3/${INFURO_API_KEY}`,
-            accounts: [`0x${PRIV_1}`]
-        },
-        bsc_testnet: {
-            url: `https://data-seed-prebsc-1-s3.binance.org:8545/`,
-            accounts: [`0x${PRIV_1}`]
-        },
         mumbai: {
-            url: 'https://matic-mumbai.chainstacklabs.com',
-            accounts: [`0x${PRIV_1}`, `0x${PRIV_2}`, `0x${PRIV_3}`],
-            gasPrice: 1000000000
+            url: 'https://matic-mumbai.chainstacklabs.com'
         }
     }
 };
